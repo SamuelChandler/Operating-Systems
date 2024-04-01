4fe16ef0b3f2788a0d421faf3ee20c2340dccc33
SamuelChandler <sec180005@utdallas.edu>
2024-04-01 14:14:22 -0500

    updated gitignore and starting project

--- Project1/quickSortImp.java
+++ Project1/quickSortImp.java
@@ -8,8 +8,36 @@ of the pivot value.
 when the list to be sorted
 falls within some threshold value (for example, the list is size 100 or fewer, you
 cannot hard code how many times a new thread is created), directly apply a
-simple algorithm such as the Selection or Insertion sort. 
+simple algorithm such as the Selection or Insertion sort.
  */
+
+
+
+//split on the last element
+
 public class quickSortImp {
-    
+
+    //variable used for testing
+    int[] inputData= {1,7,3,5,4,6,2};
+
+    //pivot point of the array for the quick sort
+    int pivot = inputData[inputData.length-1];
+
+    //the results storing the split array
+    int[] lowerArray;
+    int[] higherArray = {};
+
+
+    //iterate through each element of the input Data
+    for(int x = 0; x <inputData.length-1;x++){
+
+    }
+
+
+
 }
+
+//next steps:
+// 1 create quick sort base without forking [] 
+// 2 implement with forming and window size []
+// 3 integrate Random int generator         []
