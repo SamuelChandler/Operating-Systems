c9f77de2c04f527e9b03912b0b149fad12e49869
SamuelChandler <sec180005@utdallas.edu>
2024-04-02 18:02:02 -0500

    made split and merge into function

--- Project1/quickSortImp.java
+++ Project1/quickSortImp.java
@@ -12,49 +12,58 @@ simple algorithm such as the Selection or Insertion sort.
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 //split on the last element
 
 public class quickSortImp {
 
-    public static void main(String[] args){ 
-    //variable used for testing
-        int inputData[]= {1,7,3,5,4,6,2};
-
-    //pivot point of the array for the quick sort
-        int pivot = inputData[inputData.length-1];
-
-        //the results storing the split array
-        List<Integer> higher = new ArrayList<Integer>(), lower = new ArrayList<Integer>();
-
-        //resulting list after recombination 
-        List<Integer> result = new ArrayList<Integer>();
-
-    //iterate through each element of the input Data
-        for(int x = 0; x <inputData.length;x++){
-            //if higher than pivot then add to higher array else to lower
-            if(inputData[x]<=pivot){
-                lower.add(inputData[x]);
-            }else{
-                higher.add(inputData[x]);
+    public static int[] SplitAndMerge(int[] args){ 
+    
+        //pivot point of the array for the quick sort
+            int pivot = args[args.length-1];
+    
+            //the results storing the split array
+            List<Integer> higher = new ArrayList<Integer>(), lower = new ArrayList<Integer>();
+    
+            //resulting list after recombination 
+            List<Integer> result = new ArrayList<Integer>();
+    
+        //iterate through each element of the input Data
+            for(int x = 0; x <args.length;x++){
+                //if higher than pivot then add to higher array else to lower
+                if(args[x]<=pivot){
+                    lower.add(args[x]);
+                }else{
+                    higher.add(args[x]);
+                }
             }
+    
+            //print values to check 
+            System.err.println("data: "+ args);
+            System.out.println(pivot);
+            System.out.println("Lower: "+lower.toString());
+            System.out.println("Higher: " + higher.toString());
+    
+            // adding the results to the split
+            result.addAll(lower);
+            result.addAll(higher);
+    
+            //print result
+            return(result.stream().mapToInt(Integer::intValue).toArray());
+    
         }
 
-        //print values to check 
-        System.err.println("data: "+ inputData);
-        System.out.println(pivot);
-        System.out.println("Lower: "+lower.toString());
-        System.out.println("Higher: " + higher.toString());
+    public static void main(String[] args){ 
+        int[] inputData= {1,7,3,5,4,6,2};
 
-        // adding the results to the split
-        result.addAll(lower);
-        result.addAll(higher);
+        int[] res = SplitAndMerge(inputData);
 
-        //print result
-        System.out.println("Result: "+result.toString());
-
+        System.out.println(Arrays.toString(res));
     }
+
+    
 }
 
 
