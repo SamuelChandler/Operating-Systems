67e8b65e608a5f1350a9eeb39e01b3b1bad55cd7
SamuelChandler <sec180005@utdallas.edu>
2024-04-02 17:38:17 -0500

    basic split and merge

--- Project1/quickSortImp.java
+++ Project1/quickSortImp.java
@@ -11,31 +11,52 @@ cannot hard code how many times a new thread is created), directly apply a
 simple algorithm such as the Selection or Insertion sort.
  */
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 //split on the last element
 
 public class quickSortImp {
 
+    public static void main(String[] args){ 
     //variable used for testing
-    int[] inputData= {1,7,3,5,4,6,2};
+        int inputData[]= {1,7,3,5,4,6,2};
 
     //pivot point of the array for the quick sort
-    int pivot = inputData[inputData.length-1];
+        int pivot = inputData[inputData.length-1];
 
-    //the results storing the split array
-    int[] lowerArray;
-    int[] higherArray = {};
+        //the results storing the split array
+        List<Integer> higher = new ArrayList<Integer>(), lower = new ArrayList<Integer>();
 
+        //resulting list after recombination 
+        List<Integer> result = new ArrayList<Integer>();
 
     //iterate through each element of the input Data
-    for(int x = 0; x <inputData.length-1;x++){
+        for(int x = 0; x <inputData.length;x++){
+            //if higher than pivot then add to higher array else to lower
+            if(inputData[x]<=pivot){
+                lower.add(inputData[x]);
+            }else{
+                higher.add(inputData[x]);
+            }
+        }
+
+        //print values to check 
+        System.err.println("data: "+ inputData);
+        System.out.println(pivot);
+        System.out.println("Lower: "+lower.toString());
+        System.out.println("Higher: " + higher.toString());
+
+        // adding the results to the split
+        result.addAll(lower);
+        result.addAll(higher);
+
+        //print result
+        System.out.println("Result: "+result.toString());
 
     }
-
-
-
 }
+
 
 //next steps:
 // 1 create quick sort base without forking [] 
